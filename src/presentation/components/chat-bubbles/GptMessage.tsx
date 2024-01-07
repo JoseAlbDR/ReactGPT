9ae6df9f8a62e8558666c5646a980fdd0120431d
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-01-07 17:48:44 +0100

    style: reestyle chat components

--- src/presentation/components/chat-bubbles/GptMessage.tsx
+++ src/presentation/components/chat-bubbles/GptMessage.tsx
@@ -11,9 +11,9 @@ const GptMessage = ({ text }: Props) => {
         <div className="flex items-center justify-center h-10 w-10 rounded-full bg-green-600 flex-shrink-0">
           G
         </div>
-      </div>
-      <div className="relative ml-3 text-sm bg-black bg-opacity-25 pt-3 pb-2 px-4 shadow rounded-xl">
-        <Markdown>{text}</Markdown>
+        <div className="relative ml-3 text-sm bg-black bg-opacity-25 pt-3 pb-2 px-4 shadow rounded-xl">
+          <Markdown>{text}</Markdown>
+        </div>
       </div>
     </div>
   );
--- src/presentation/components/chat-bubbles/UserMessage.tsx
+++ src/presentation/components/chat-bubbles/UserMessage.tsx
@@ -9,9 +9,9 @@ const MyMessage = ({ text }: Props) => {
         <div className="flex items-center justify-center h-10 w-10 rounded-full bg-purple-500 flex-shrink-0">
           U
         </div>
-      </div>
-      <div className="relative mr-3 text-sm bg-purple-700 py-2 px-4 shadow rounded-xl">
-        {text}
+        <div className="relative mr-3 text-sm bg-purple-700 py-2 px-4 shadow rounded-xl">
+          {text}
+        </div>
       </div>
     </div>
   );
