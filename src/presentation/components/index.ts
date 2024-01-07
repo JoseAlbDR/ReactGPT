8d0b3336c1e5718353ad042ef9294aa081b5755f
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-01-07 17:51:15 +0100

    feat: TypingLoader component

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,3 +1,4 @@
 export { default as SidebarItem } from '../components/sidebar/SidebarItem';
 export { default as GptMessage } from '../components/chat-bubbles/GptMessage';
 export { default as UserMessage } from './chat-bubbles/UserMessage';
+export { default as TypingLoader } from '../components/loaders/TypingLoader';
