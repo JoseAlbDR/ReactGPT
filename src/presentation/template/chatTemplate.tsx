c46f83a05595ae6c857f82565e71f0af6966d330
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-01-08 11:15:35 +0100

    feat: ChatTemplate done

---
+++ src/presentation/template/chatTemplate.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import {
+  GptMessage,
+  UserMessage,
+  TypingLoader,
+  TextMessageBox,
+} from '../components';
+
+interface Message {
+  text: string;
+  isGpt: boolean;
+}
+
+const ChatTemplate = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async (text: string) => {
+    setIsLoading(true);
+    setMessages((prev) => [...prev, { text, isGpt: false }]);
+
+    //TODO: UseCase
+
+    setIsLoading(false);
+
+    //TODO: Add message isGpt true
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          <GptMessage text="Hello there!, you can write your text in english and I will help you with the corrections" />
+
+          {messages.map((message, index) =>
+            message.isGpt ? (
+              <GptMessage key={index} text="OpenAI!" />
+            ) : (
+              <UserMessage key={index} text={message.text} />
+            )
+          )}
+
+          {isLoading && (
+            <div className="col-start-1 col-end-12 fade-in">
+              <TypingLoader />
+            </div>
+          )}
+        </div>
+      </div>
+
+      <TextMessageBox
+        onSendMessage={handlePost}
+        placeholder="Write here your shit"
+        disableCorrections
+      />
+    </div>
+  );
+};
+
+export default ChatTemplate;
